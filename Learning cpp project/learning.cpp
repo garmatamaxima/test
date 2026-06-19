fd2884e7b60492fe46635a3a1f0e57c5198a0fd5
garmatamaxima <garmatamaxima@gmail.com>
2026-06-19 18:34:06 +0300

    ok2

--- Learning cpp project/learning.cpp
+++ Learning cpp project/learning.cpp
@@ -1,4 +1,6 @@
 
+//To use the functionality defined within the iostream library, we need to include the iostream header at the top of any code file 
+// that uses the content defined in iostream
 #include <iostream>
 
 /*
@@ -9,25 +11,19 @@
     Values that are placed directly into the source code are called literals
     
     // ---- OBJECTS: ----
-    In C++, we use objects to access memory. A named object is called a variable. Each variable has an identifier (a name), a type, and a value
+    In C++, we use objects to access memory. A named object is called a variable. Each variable has an identifier (a name), a type, 
+     and a value
     A variable’s type is used to determine how the value in memory should be interpreted.
-
-
+    
+    // ---- INSTANTIATION: ----
+    The term instantiation is a fancy word that means a variable has been created (allocated) 
+    and initialized (this includes default initialization)
 */
 
 
+void asignment_and_initalisation() {
 
-// attempting to initalise a float when integer was specified using list initalisation, forces compiler error
-void exampleBUG_narrowing_conversion() {
-
-    int bug_value{ 1.1 };
-
-}
-
-
-
-
-void tutorial_asignment_and_initalisation() {
+    std::cout << "/ This is Assignment and Initalisation outputs: \n";
 
     // ---- DIRECT LIST INITALISATION, or just LIST INITALISATION: ----
 
@@ -40,7 +36,14 @@ void tutorial_asignment_and_initalisation() {
     // (Plain english) -- define a variable named "first" of type integer then initalise it with initial value 1.
     int first {1};
 
-    std::cout << first << "\n";
+    std::cout << "list init >>" << first << "\n";
+
+    // ---- ZERO INITALISATION, VALUE INITALISATION: ----
+    
+    int width{}; // will implicitly initialize the variable to zero
+
+    std::cout << "zero init >> " << width << "\n";
+
 
     // ---- COPY LIST INITALISATION: ----
 
@@ -49,7 +52,7 @@ void tutorial_asignment_and_initalisation() {
     // (Plain english) -- define a variable named "alpha" of type integer then initalise it with initial value 1.
     int alpha = { 1 };
 
-    std::cout << alpha << "\n";
+    std::cout << "copy list init >> " << alpha << "\n";
 
     // ---- COPY INITALISATION: ----
     
@@ -57,15 +60,15 @@ void tutorial_asignment_and_initalisation() {
     // (Plain english) -- define a variable named "second" of type integer then initalise it with initial value of 1
     int second = 1;
 
-    std::cout << second << "\n";
+    std::cout << "copy init >> " << second << "\n";
 
     // ---- DIRECT INITALISATION: ----
 
-    // different syntax for initalising a variable
+    // Potentially dangerous!
     // (Plain english) -- define a variable named "third" of type integer then initalise it with initial value of 1
     int third (1);
 
-    std::cout << third << "\n";
+    std::cout << "direct init >> " << third << "\n";
 
     // ---- ASSIGNMENT: ----
 
@@ -73,45 +76,66 @@ void tutorial_asignment_and_initalisation() {
     // (Plain english) -- variable "first" is equal to 3
     first = 3;
 
+    std::cout << "Assignment of variable to different value >> " << first << "\n";
+
     // ---- IMPORTANT TO KNOW!!!: ----
     
     /* (Narrowing conversions) --
 
     * Initializing an int with fractional value 4.5 requires the compiler to convert 4.5 to a value an int can hold
-    * such a conversion is a narrowing conversion, since the fractional part of the value will be lost, compiler will gladly convert 4.5 to 4.
+    * such a conversion is a narrowing conversion, since the fractional part of the value will be lost, compiler will gladly convert
+     4.5 to 4.
     * LIST INITALISATION will throw an compile error, because it does not allow it.
 
     */
 
 
+}
 
+void may_be_unused() {
 
-    std::cout << first << "\n";
+    // ---- MAY BE UNUSED: ----
+
+    /*
+    Some values can be unused which is undesirable
+    But sometimes you can have a list of variables that have to be saved for later use
+    */
+    // [[maybe_unused]] is an attribute, which tells compuler that variable is okay to be unused
+    [[maybe_unused]] double pi{ 3.14159 };
+    [[maybe_unused]] double randomsomething{ 2323.54359 };
+    [[maybe_unused]] int one{ 1 }, two{ 2 }, three{ 3 };
+    
+    // legal but bad practice
+    int i_am_useless{ 1 };
 
 
 
 }
 
-void tutorialcout() {
 
-    // "std::cout" is a statement (оператор), std:: means we take a function from standart library, cout is the part of iostream library inside standart library
-    // "cout" stands for "Character Output"
-    // "<<" operator allows to display information on console and concatenate (connect strings together, обєднувати рядки у один)
-    // "\n" is a format specifier, it allows for walking to a next string in console 
-    std::cout << "Hello World!\n";
-    std::cout << "Українською " << "також!\n"; // C++ компілятор (compiler) української мови не знає
-    std::cout << 'A' << "\n";
+void iostream_tutorial() {
 
+    float a_variable{123.233412342};
 
+    /*
+    "std::cout" is a statement (оператор), std:: means we take a function from standart library, cout is the part of iostream library
+     inside standart library
+    "cout" stands for "Character Output"
+    "<<" INSERTION OPERATOR: allows to display information on console and concatenate (connect strings together, обєднувати рядки у один)
+    "\n" is a format specifier, it allows for walking to a next string in console, moves console cursor to new line
+    "std::endl" also moves console cursor to new line + flushes the buffer of a console 
+    */
+    std::cout << "Hello World!, i am std::cout function!" << std::endl;
+    std::cout << "numbers... " << 1 << ", " << 1.001 << ", " << a_variable << "\n";
 }
 
 
 int main()
 {   
-    tutorialcout();
-    
-    
-    tutorial_asignment_and_initalisation();
+    iostream_tutorial();
+    asignment_and_initalisation();
+    may_be_unused();
+
 
     return 0;
 }
--- example bugs cpp/example bugs.cpp
+++ example bugs cpp/example bugs.cpp
@@ -2,21 +2,67 @@
 /*
 =============================
 breaking code since 2026 
-drop code snippets from this file into your program to kill it brutally
+drop code snippets from this file into your program to kill it brutally or make compiler cry like a bitch
 =============================
 */
 
 
-// ---- COMPILE ERRORS: ----
 
-// attempting to initalise a float when integer was specified using list initalisation. Compiler throws an error 
-int BUG_narrowing_conversion_rejection() {
 
-	int alpha{ 6.111 };
 
+// ---- INITALISATION ERRORS: ----
+
+/*
+2026.06.19
+learncpp chapter 1.4
+Using uninitalised value that will produce garbage data and undefined behaviours, compiler catches it usually.
+OR Default initalisation leaves the variable with an indeterminate (random) value, very bad practice
+*/
+void BUG_garbage_initalisation() {
+
+    // When no initaliser is provided for initalising a variable, it will hold a random value that was sitting in that memory adress
+    float garbage;
+    std::cout << garbage;
+    
 }
 
+// GOOD PRACTICE:
+void good_initalisation() {
+
+    // List initalisation
+    float not_garbage{0.00};
+    std::cout << not_garbage;
+    
+}
+//----
+
+/*
+2026.06.15
+learncpp chapter 1.4
+Attempting to initalise a float when integer was specified using list initalisation. Compiler throws an error
+*/
+int BUG_narrowing_conversion() {
+
+    // compiler will kill that one, good bug
+    int alpha{ 6.111 };
+    // value will be truncated and set to 0, bad bug
+    int beta = 0.01;
+}
+// GOOD PRACTICE:
+void narrowing_conversion_prevention() {
+    
+    // just use list initalisation, errors will be caught at compile time usually
+    int theta{ 1 };
+    // when you want to truncate it deliberately, its fine
+    int omega = 1.1;
+
+}
+// ----
+
+
+
 // must have A SINGLE main function in a program, if you have no main() then program wont compile
+// returning non 0 value is a bad practice when program finishes properly
 int main() {
 
 

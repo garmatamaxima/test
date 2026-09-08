52e1f91a09ca1f7d8e6754d4d38e3fc66679ba2d
garmatamaxima <garmatamaxima@gmail.com>
2026-09-09 02:06:32 +0300

    123

--- Source code/lab/game/main.cpp
+++ Source code/lab/game/main.cpp
@@ -2,22 +2,31 @@
 #include "raylib.h"
 #include "raymath.h"
 #include "rlgl.h"
+
 #include <cmath>
+#include <string>
+#include <vector>
+#include <array>
 
 #include "main.h"
+
+using namespace mainGlobals;
 
 class entity
 {
 public:
+
+
 	Vector2 position;
 	Vector2 direction;
 	Vector2 velocity;
 	float angleRad;
-	
+	bool status;
+
 	int id;
 	int size;
 
-	void init(float posX, float posY, int id_, int size_ = 10, float angle = 0)
+	void init(float posX, float posY, int id_, int size_ = 10, float angle = 0, bool status_ = true)
 	{
 		position.x = posX;
 		position.y = posY;
@@ -27,6 +36,7 @@ public:
 		direction.y = std::sin(angle);
 		id = id_;
 		size = size_;
+		status = status_;
 
 		velocity.x = 0;
 		velocity.y = 0;
@@ -37,29 +47,32 @@ public:
 		position.x += velocity.x;
 		position.y += velocity.y;
 	}
-	
+
+
+
 	void setVelocity(const Vector2& velocity_vector)
 	{
 		velocity = velocity_vector;
 	}
-	void setVelocity( float x, float y )
+	void setVelocity(float x, float y)
 	{
 		velocity.x = x;
 		velocity.y = y;
 	}
 
-	void accelerate( const Vector2& velocity_vector )
+	void accelerate(const Vector2& velocity_vector)
 	{
 		velocity.x += velocity_vector.x;
 		velocity.y += velocity_vector.y;
 	}
-	void accelerate( float x, float y )
+	void accelerate(float x, float y)
 	{
 		velocity.x += x;
 		velocity.y += y;
+
 	}
 
-	void accelerateDirection(float acceleration )
+	void accelerateDirection(float acceleration)
 	{
 		velocity.x += direction.x * acceleration;
 		velocity.y += direction.y * acceleration;
@@ -71,8 +84,8 @@ public:
 		direction = Vector2Normalize(Vector2Subtract(mousePos, position));
 		angleRad = std::atan2(direction.y, direction.x);
 	}
-	
-	void pointToTarget( const Vector2& target )
+
+	void pointToTarget(const Vector2& target)
 	{
 		direction = Vector2Normalize(Vector2Subtract(target, position));
 	}
@@ -88,25 +101,159 @@ public:
 		direction.y = std::sin(angleRad);
 	}
 
+	void wraparoundCheck()
+	{
+		if (position.x > screenWidth)
+		{
+			position.x = 0;
+		}
+		else if (position.x < 0)
+		{
+			position.x = screenWidth;
+		}
+
+		if (position.y > screenHeight)
+		{
+			position.y = 0;
+		}
+		else if (position.y < 0)
+		{
+			position.y = screenHeight;
+		}
+
+	}
+
 };
 
-class bullet
+namespace
 {
-public:
-
-
-
-	entity positional;
-};
-
-namespace mainGlobals
-{
-	static int ids{ 0 };
-	int playermode{ 0 };
-
+	static std::vector<entity> bullets_bufferVector{};
+	static entity bullets_buffer[100];
 }
 
-using namespace mainGlobals;
+struct point
+{
+	Vector2 pos;
+	float& x{pos.x};
+	float& y{pos.y};
+
+	bool state = false ;
+};
+
+class object
+{
+public:
+	Vector2 direction;
+	Vector2 Center;
+	Vector2 velocity;
+	float angularVelocity; // in radians
+
+	std::array<point,32> points; // in local frame, they are offsets from center.
+
+	void init(float posX, float posY, float angle )
+	{
+		Center.x = posX;
+		Center.y = posY;
+		direction.x = std::cos(angle);
+		direction.y = std::sin(angle);
+		
+		for (short i{ 0 }; i < (sizeof(points) / sizeof(points[0])); ++i)
+		{
+			points[i].state = false;
+		}
+		
+		rotatePoints( angularVelocity );
+	}
+
+	void rotatePoints(float angle)
+	{
+		float cos{ std::cos(angle) };
+		float sin{ std::sin(angle) };
+		float tempx;
+		float tempy;
+
+		for (short i{ 0 }; i < (sizeof(points) / sizeof(points[0])); ++i)
+		{	
+			tempx = points[i].x;
+			tempy = points[i].y;
+
+			points[i].x = tempx * cos - tempy * sin;
+			points[i].y = tempx * sin + tempy * cos;
+		}
+	}
+
+	void rotatePoints1( )
+	{
+		float cos{ std::cos(angularVelocity) };
+		float sin{ std::sin(angularVelocity) };
+		float tempx;
+		float tempy;
+
+		for (short i{ 0 }; i < (sizeof(points) / sizeof(points[0])); ++i)
+		{
+			tempx = points[i].x;
+			tempy = points[i].y;
+
+			points[i].x = tempx * cos - tempy * sin;
+			points[i].y = tempx * sin + tempy * cos;
+		}
+	}
+
+	void resetPoints()
+	{
+		for (short i{ 0 }; i < (sizeof(points) / sizeof(points[0])); ++i)
+		{
+			points[i].x = 0;
+			points[i].y = 0;
+			points[i].state = false;
+		}
+	}
+
+	void render()
+	{
+		short lineNextIndex;
+
+		DrawCircle( Center.x, Center.y, 5, BLUE );
+		for (short i{ 0 }; i < ( sizeof(points) / sizeof(points[0]) ) ; ++i)
+		{
+			if (points[i].state == true)
+			{
+				lineNextIndex = (i + 1) % 32;
+
+				DrawLine(points[i].x + Center.x, points[i].y + Center.y, points[lineNextIndex].x + Center.x, points[lineNextIndex].y + Center.y, BLACK);
+			}
+			DrawCircle( points[i].x + Center.x, points[i].y + Center.y, 5, GREEN );
+		}
+	}
+
+	Vector2 GlobalToLocal(const Vector2& global_point)
+	{
+		return Vector2Subtract( global_point, Center );
+	}
+
+	void createPoint(const Vector2& global_framePoint)
+	{
+		for (short i{ 0 }; i < (sizeof(points) / sizeof(points[0])); ++i)
+		{
+			if (points[i].state == false)
+			{
+				points[i].state = true;
+				points[i].pos = GlobalToLocal( global_framePoint ) ;
+				return;
+
+			}
+		}
+	}
+
+
+};
+
+
+
+namespace obj
+{
+	object rectangle{};
+}
 
 int getNewId()
 {
@@ -115,22 +262,27 @@ int getNewId()
 	return temp;
 }
 
-
 void drawPointer( const entity entity )
 {
 	DrawCircle ( entity.position.x + entity.direction.x * 20, entity.position.y + entity.direction.y * 20, 5, DARKGRAY );
 }
 
-
 void playerInput( entity& player, float speed )
 {
+	if (IsKeyPressed(KEY_Q)) 
+	{
+		if (playermode != 1 ) { playermode = 1; }
+		else { playermode = 0; }
+	}
+
 	if (IsKeyDown(KEY_A)) {
 		
 		if (playermode == 0)
 		{
 			player.accelerate(-(dt * speed), 0);
 		}
-		else { player.velocity.x = -(dt * speed); }
+		else {
+			player.position.x += dt * speed * -34.5; player.setVelocity(0, 0); }
 	}
 	else if (IsKeyDown(KEY_D))
 	{
@@ -138,7 +290,8 @@ void playerInput( entity& player, float speed )
 		{
 			player.accelerate(dt * speed, 0);
 		}
-		else { player.velocity.x = dt * speed; }
+		else { player.position.x += dt * speed * 34.5; player.setVelocity(0, 0);
+		}
 	}
 
 	if (IsKeyDown(KEY_W)) 
@@ -147,7 +300,7 @@ void playerInput( entity& player, float speed )
 		{
 			player.accelerate(0, -(dt * speed));
 		}
-		else { player.velocity.y = -(dt * speed); }
+		else { player.position.y += -(dt * speed * 34.5); player.setVelocity(0,0); }
 	}
 	else if (IsKeyDown(KEY_S))
 	{
@@ -155,76 +308,124 @@ void playerInput( entity& player, float speed )
 		{
 			player.accelerate(0, (dt * speed));
 		}
-		else { player.velocity.y = dt * speed; }
+		else { player.position.y += dt * speed * 34.5; player.setVelocity(0, 0); }
 	}
 }
 
-void shoot(const entity& player)
+void shootInputChecker(const entity& player, float speed )
 {
-	if 
+	int static idSelector{0};
+
+	if (IsKeyPressed(KEY_SPACE))
+	{
+		if (idSelector > 99 || idSelector < 0) { idSelector = 0; }
+
+		bullets_buffer[idSelector].status = true;
+		
+		bullets_buffer[idSelector].position.x = player.position.x + player.direction.x * 25;
+		bullets_buffer[idSelector].position.y = player.position.y + player.direction.y * 25;
+
+		bullets_buffer[idSelector].setVelocity( player.direction.x * speed * dt + player.velocity.x, player.direction.y * speed * dt + player.velocity.y);
+
+		idSelector += 1;
+	}
+
+	if (IsKeyPressed(KEY_LEFT_SHIFT))
+	{
+		obj::rectangle.createPoint( GetMousePosition());
+	}
+	if (IsKeyPressed(KEY_R))
+	{
+		obj::rectangle.resetPoints();
+	}
+
+	if (IsKeyPressed(KEY_F))
+	{
+		obj::rectangle.angularVelocity = obj::rectangle.angularVelocity + 0.02;
+		if (obj::rectangle.angularVelocity > 0.04 || obj::rectangle.angularVelocity < 0)
+		{
+			obj::rectangle.angularVelocity = 0;
+		}
+	}
 }
 
+
+
+
+void renderProjectiles()
+{
+	int bulletsActive{};
+
+	for (int i1{ 0 }; i1 < 100; ++i1)
+	{
+		if (bullets_buffer[i1].status == true)
+		{
+			bulletsActive += 1;
+
+			bullets_buffer[i1].updatePosition();
+			bullets_buffer[i1].wraparoundCheck();
+
+			DrawCircle( bullets_buffer[i1].position.x, bullets_buffer[i1].position.y , bullets_buffer[i1].size, RED );
+		}
+	}
+	DrawText(TextFormat("Bullets created: %i", bulletsActive), 55, 55, 25, BLACK);
+}
 
 int main()
 {
 	InitWindow(screenWidth, screenHeight, "PROGRAM");
 	SetTargetFPS(120);
 
-	Vector2 deltaCircle = { 0, (float)screenHeight / 3.0f };
-	Vector2 frameCircle = { 0, (float)screenHeight * (2.0f / 3.0f) };
 
+	// initalising both player and bullet entities
 	entity player1{ }; player1.init(screenWidth/2, screenHeight/2, getNewId(), 15, 0);
 
-	entity bullets[32] {};
+	for (int i1{ 0 }; i1 < 100; ++i1)
+	{
+		bullets_buffer[i1].init(0, 0, getNewId(), 5, 0, false);
+	}
 
-	const float speed = 50.0f;
-	const float circleRadius = 12.0f;
+	int coom[100] {};
+
+	obj::rectangle.init(screenWidth / 2, screenHeight / 2, 1.5);
+	obj::rectangle.angularVelocity = 0.001;
 	
+	/// main loop
 	while (!WindowShouldClose()) // detects ESC.
 	{
+		//
 		dt = GetFrameTime();
 		mousePos = GetMousePosition();
 
 		/// logic part
-		
-		if (deltaCircle.x > screenWidth + 15) deltaCircle.x = 0;
-		if (frameCircle.x > screenWidth + 15) frameCircle.x = 0;
-
-		deltaCircle.x += dt * speed;
-		frameCircle.x += 0.1f * speed;
 
 		playerInput( player1 , 10.0f );
 		player1.pointToMouse();
 		player1.updatePosition();
 		
-		if (player1.position.x > screenWidth + 10) { player1.position.x = 0; }
-		else if (player1.position.x < 0 ) { player1.position.x = screenWidth + 10; }
 
-		if (player1.position.y > screenHeight + 10) { player1.position.y = 0; }
-		else if (player1.position.y < 0) { player1.position.y = screenHeight - 10; }
+		player1.wraparoundCheck();
+		shootInputChecker(player1, 325);
 
+		obj::rectangle.rotatePoints1();
 
 		/// Render part
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 
+		obj::rectangle.render();
+
+		renderProjectiles();
+
+		// text that appears when user hovers their mouse over player
 		if ( Vector2Distance( mousePos, player1.position ) < player1.size + 5 )
 		{
 			DrawText("you", player1.position.x - 14, player1.position.y - 44, 18, BLACK);
 		}
-
 		//player
 		DrawCircleV(player1.position, player1.size, BLACK);
 		drawPointer( player1 );
-
-
-		//circles
-		DrawCircleV(deltaCircle, circleRadius, RED);
-		DrawCircleV(frameCircle, circleRadius, BLUE);
-
-		// virtual buttons
-
-
+		
 		EndDrawing();
 	}
 
